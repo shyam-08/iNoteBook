8c0689470a2f11722ff231c38e7c23a893bc6d30
shyam-08 <65022206+shyam-08@users.noreply.github.com>
2023-08-02 23:12:53 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
-// conection string for mango db 
-const mongoURI ="mongodb://localhost:27017/inotebook&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
-mongodb://localhost:27017/
-
-   var  connectToMongo =  () => {
-        mongoose.connect(mongoURI,()=>{
-    
-           console.log("Connected to mongo Successful")
-       
-   })
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
 }
-   
-module.exports =connectToMongo ;
+
+module.exports = connectToMongo;
